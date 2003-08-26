15185bfb33c212fdc9b6abf305368f691bcad5f9
carrott <>
2003-08-26 09:02:06 +0000

    made unimplemented functions fail asserts

--- nsprpub/pr/src/md/amiga/athreads.c
+++ nsprpub/pr/src/md/amiga/athreads.c
@@ -46,42 +46,68 @@
 _PRInterruptTable _pr_interruptTable[] = {};
 
 void _InitCpus() {
+#warning unimplemented function
+   assert(0);
 }
 
 void _WakeupCpus() { 
+#warning unimplemented function
+   assert(0);
 }
 
 void _StartInterrupts(void) { 
+#warning unimplemented function
+   assert(0);
 }
 
 void _StopInterrupts(void) { 
+#warning unimplemented function
+   assert(0);
 }
 
 void _EnableClockInterrupts(void) { 
+#warning unimplemented function
+   assert(0);
 }
 
 void _DisableClockInterrupts(void) { 
+#warning unimplemented function
+   assert(0);
 }
 
 void _BlockClockInterrupts(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _UnblockClockInterrupts(void) { 
+#warning unimplemented function
+   assert(0);
 }
 
 void _ClockInterrupt(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRStatus _Wait(PRThread *thread, PRIntervalTime timeout) { 
+#warning unimplemented function
+   assert(0);
 }
 
 PRStatus _WakeupWaiter(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _InitStack(PRThreadStack *ts, PRIntn redzone) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _ClearStack(PRThreadStack* ts) {
+#warning unimplemented function
+   assert(0);
 }
 
 
@@ -100,148 +126,241 @@ void _InitRunningCpu(_PRCPU *cpu) {
 	assert(0);
 }
 
-
 PRInt32 _PauseCpu(PRIntervalTime timeout) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _CleanupBeforeExit(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _Exit(PRIntn status) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _InitLocks(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRStatus _NewLock(struct _MDLock *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _FreeLock(struct _MDLock *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _Lock(struct _MDLock *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRIntn _TestAndLock(struct _MDLock *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _UnLock(struct _MDLock *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _IOQLock(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _IOQUnLock(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _NewSem(struct _MDSemaphore *md, PRUintn value) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _DestroySem(struct _MDSemaphore *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRStatus _TimedWaitSem(struct _MDSemaphore *md, PRIntervalTime timeout) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRStatus _WaitSem(struct _MDSemaphore *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _PostSem(struct _MDSemaphore *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRInt32 _NewCV(struct _MDCVar *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _FreeCV(struct _MDCVar *md) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _WaitCV(struct _MDCVar *mdCVar, struct _MDLock *mdLock, PRIntervalTime timeout) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _NotifyCV(struct _MDCVar *md, struct _MDLock *lock) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _NotifyAllCV(struct _MDCVar *md, struct _MDLock *lock) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRThread *_GetAttachedThread(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRThread *_LastThread(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _SetCurrentThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _SetLastThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRStatus _InitThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _ExitThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRStatus _InitAttachedThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _SuspendThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _ResumeThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 /*
 void _SuspendCpu(_PRCPU  *cpu) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _ResumeCpu(_PRCPU  *cpu) {
+#warning unimplemented function
+   assert(0);
 }
 */
 
 void _BeginSuspendAll(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _EndSuspendAll(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _BeginResumeAll(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _EndResumeAll(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _CleanThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _CreatePrimordialUserThread(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRThread *_CreateUserThread(PRUint32 stacksize, void (*start)(void *), void *arg) {
+#warning unimplemented function
+   assert(0);
 }
 
 PRStatus _CreateThread(PRThread *thread, void (*start)(void *), PRThreadPriority priority, PRThreadScope scope, PRThreadState state, PRUint32 stackSize) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _Yield(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _SetPriority(struct _MDThread *md, PRThreadPriority newPri) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _SuspendAll(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _ResumeAll(void) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _InitContext(PRThread *thread, char *top, void (*start) (void), PRBool *status) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _SwitchContext(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 void _RestoreContext(PRThread *thread) {
+#warning unimplemented function
+   assert(0);
 }
 
 /* Process control */
