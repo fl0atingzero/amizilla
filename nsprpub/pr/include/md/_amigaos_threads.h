b4326e710f62d5d347bcf791d2fc56fc91133c82
jshepher <>
2004-05-09 16:06:50 +0000

    _PR_MD_Wait now takes a flag indicating that the wait can be interrupted.

--- nsprpub/pr/include/md/_amigaos_threads.h
+++ nsprpub/pr/include/md/_amigaos_threads.h
@@ -50,7 +50,7 @@
 /* Threads related */
 #define _MD_CURRENT_THREAD                PR_GetCurrentThread
 
-void _PR_MD_Wait(PRThread *thread);
+void _PR_MD_Wait(PRThread *thread, PRBool interruptable);
 void _PR_MD_Signal(PRThread *thread);
 
 struct _MDProcess {
