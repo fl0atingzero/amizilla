14e10f1a4fcebf2f250bd330d8dea1433f6f47d2
jshepher <>
2004-05-09 16:21:10 +0000

    Keep track of the primordial thread and only let that thread do PR_Cleanup. In PR_Interrupt, make sure the thread exists (since the pointer maybe bogus). Keep track of all the threads. Implement interrupts. PR_MD_Wait now takes a interrupt flag saying the Wait() can be interrupted.

--- nsprpub/pr/src/md/amiga/athreads.c
+++ nsprpub/pr/src/md/amiga/athreads.c
@@ -37,22 +37,49 @@
 
 static PRStatus _InitThread(PRThread *pr);
 
-
 void _Exit(PRIntn status) {
     /* TODO - how to signal all the threads to stop!! */
 }
 
+static PRThread *primordialThread = NULL;
 
 void _MD_Early_Init(void) {
-    PRThread *thread = PR_NEWZAP(PRThread);
+    PRThread *thread;
+    thread = PR_NEWZAP(PRThread);
+    primordialThread = thread;
     thread->p = (struct Process *)FindTask(NULL);
     thread->p->pr_Task.tc_UserData = thread;
     _InitThread(thread);
+    /* For debugging */
+    setvbuf(stdout, NULL, _IONBF, 0);
+    printf("Primorial Thread %lx\n", thread);
     atexit(PR_Cleanup);
 }
 
 PR_IMPLEMENT(PRStatus) PR_Interrupt(PRThread *thread) {
-    Signal(thread->p, 1<< thread->port->mp_SigBit);
+    PRThread *me = PR_CurrentThread();
+    char buf[50];
+    struct MsgPort *port;
+    sprintf(buf, "NSPRPORT-%lx\n", thread);
+    /* See if the thread is still around by trying to 
+     * look for its public message port 
+     */
+    Forbid();
+    port = FindPort(buf);
+    if (port == NULL) {
+        Permit();
+        return PR_SUCCESS;
+    }
+
+    PR_Lock(thread->stateLock);
+    thread->flags |= _PR_INTERRUPT;
+    printf("%lx Interrupting thread %lx, flags are now %lx\n", me, thread, thread->flags);
+    PR_Unlock(thread->stateLock);
+    if (me != thread) {
+        Signal((struct Task *)thread->p, 1 << thread->interruptSignal);
+    }
+    Permit();
+        
     return PR_SUCCESS;
 }
 
@@ -62,10 +89,15 @@ PR_IMPLEMENT(PRStatus) PR_Interrupt(PRThread *thread) {
 static PRStatus _InitThread(PRThread *pr) {
     char *buf = PR_Malloc(50);
     sprintf(buf, "NSPRPORT-%lx\n", pr);
-    pr->port = CreatePort(buf, 0);
+    pr->port = CreateMsgPort();
     PR_ASSERT(pr->port);
+    pr->port->mp_Node.ln_Name = buf;
+    AddPort(pr->port);
     pr->selectPort = CreateMsgPort();
     PR_ASSERT(pr->selectPort);
+    pr->interruptSignal = AllocSignal(-1);
+    PR_ASSERT(pr->interruptSignal != -1);
+
     pr->sleepRequest =
         (struct timerequest *)CreateExtIO(pr->port, sizeof(struct timerequest));    
     PR_ASSERT(pr->sleepRequest);
@@ -76,6 +108,10 @@ static PRStatus _InitThread(PRThread *pr) {
     }
 
     pr->AmiTCP_Base = OpenLibrary("bsdsocket.library", 0);
+
+    PR_INIT_CLIST(&pr->lockList);
+    pr->stateLock = PR_NewLock();
+    PR_ASSERT(pr->stateLock);
     return PR_SUCCESS;
 }
 
@@ -89,19 +125,31 @@ static void procExit(void) {
     Forbid();
     RemPort(me->port);
     join = me->join;
+    if (me->prev) {
+        me->prev->next = me->next;
+    }
+    if (me->next) {
+        me->next->prev = me->prev;
+    }
     Permit();
+
     _PR_DestroyThreadPrivate(me);
     PR_Free(me->port->mp_Node.ln_Name);
-    DeletePort(me->port);
+    DeleteMsgPort(me->port);
     DeleteMsgPort(me->selectPort);
+    FreeSignal(me->interruptSignal);
     CloseDevice((struct IORequest *)me->sleepRequest);
     DeleteExtIO((struct IORequest *)me->sleepRequest);
     if (me->AmiTCP_Base != NULL)
         CloseLibrary(me->AmiTCP_Base);
 
     if (join != NULL) {
+        printf("%lx done, signalling parent\n", me);
+        fflush(stdout);
+        PR_ASSERT(join->state == _PR_JOIN_WAIT);
         _PR_MD_Signal(join);
     }
+    PR_DestroyLock(me->stateLock);
     PR_Free(me);
 }
 
@@ -118,6 +166,7 @@ static void procEntry(void) {
     //  ixemulbase = OpenLibrary("ixemul.library", 0);
     _InitThread(pr);
     /* We are done initialization, signal the parent */
+    PR_ASSERT(pr->parent->state == _PR_SUSPENDED);
     _PR_MD_Signal(pr->parent);
     pr->startFunc(pr->arg);
     //  CloseLibrary(ixemulbase);
@@ -152,16 +201,22 @@ PR_IMPLEMENT(PRThread*) PR_CreateThread(PRThreadType type,
                                         PRThreadState state,
                                         PRUint32 stackSize) {
     PRThread *thread; 
-    PRThread *me = PR_GetCurrentThread();
+    PRThread *me;
+
     if (!_pr_initialized) _PR_ImplicitInitialization();
+
+    me = PR_CurrentThread();
     thread = PR_NEWZAP(PRThread);
+    if (thread == NULL) {
+        return NULL;
+    }
+
     thread->startFunc = start;
     thread->arg = arg;
     thread->priority = priority;
     thread->state = state;
 
     if (stackSize < 32768) stackSize = 32768;
-
     Forbid();
     thread->p = CreateNewProcTags(NP_StackSize, stackSize,
                                   NP_Name, "NSPR Thread",
@@ -180,17 +235,31 @@ PR_IMPLEMENT(PRThread*) PR_CreateThread(PRThreadType type,
         thread->p->pr_Task.tc_TrapCode = me->p->pr_Task.tc_TrapCode;
         thread->p->pr_Task.tc_TrapData = me->p->pr_Task.tc_TrapData;
         thread->parent = me;
+
+        /* Put this before signal just in case */
+        me->state = _PR_SUSPENDED;        
         /* Hack because the new thread is waiting for us to do this */
-        Signal(thread->p, SIGBREAKF_CTRL_F); 
+        Signal((struct Task *)thread->p, SIGBREAKF_CTRL_F); 
         /* Need to wait for the other thread to do some init so there
          * isn't a race condition when we do a PR_CreateThread and do 
          * a PR_JoinThread before the thread comes up fully */
-        _PR_MD_Wait(me);
+        _PR_MD_Wait(me, PR_FALSE);
+        me->state = _PR_RUNNING;
+
+        /* Add this thread to our list of threads */
+        thread->next = me->next;
+        if (me->next) {
+            me->next->prev = thread;
+        }        
+        me->next = thread;
+        thread->prev = me;
+        Permit();
     } else {
         PR_Free(thread);
         thread = NULL;
+        Permit();
     }
-    Permit();
+    printf("CreateThread(%lx) created thread %lx\n", me, thread);
     return thread;
 }
 
@@ -215,10 +284,11 @@ PR_IMPLEMENT(PRStatus) PR_JoinThread(PRThread *thread) {
         return PR_SUCCESS;
     }
     thread->join = me;
-    Permit();
 
-    thread->state = _PR_JOIN_WAIT;
-    _PR_MD_Wait(me);
+    me->state = _PR_JOIN_WAIT;
+    Permit();
+    _PR_MD_Wait(me, PR_FALSE);
+    me->state = _PR_RUNNABLE;
     return PR_SUCCESS;
 }
 
@@ -228,12 +298,52 @@ PR_IMPLEMENT(void) PR_SetConcurrency(PRUintn numCPUs) {
 
 PR_IMPLEMENT(void) PR_SetThreadPriority(PRThread *thread, PRThreadPriority priority) {
     thread->priority = priority;
-    SetTaskPri(thread->p, _PR_Map_Priority(priority));
+    SetTaskPri((struct Task *)thread->p, _PR_Map_Priority(priority));
 }
 
+PR_IMPLEMENT(void) PR_ClearInterrupt(void)
+{
+    PRThread *me = PR_CurrentThread();
+    PR_Lock(me->stateLock);
+    SetSignal(0, 1 << me->interruptSignal);
+    me->flags &= ~_PR_INTERRUPT;
+    printf("Clearing interrupt for %lx, flags are now %lx\n", me, me->flags);
+    PR_Unlock(me->stateLock);
 
-void _PR_InitThreads(
-                     PRThreadType type, PRThreadPriority priority, PRUintn maxPTDs)
+}  /* PR_ClearInterrupt */
+
+
+PR_IMPLEMENT(PRBool) PR_IsInterruptsBlocked(void)
+{
+    PRThread *me = PR_CurrentThread();
+    PRBool retval;
+    PR_Lock(me->stateLock);
+    retval = (me->flags & _PR_INTERRUPT_BLOCKED) ? PR_TRUE : PR_FALSE;
+    PR_Unlock(me->stateLock);
+    return retval;
+}
+
+PR_IMPLEMENT(void) PR_BlockInterrupt(void)
+{
+    PRThread *me = PR_CurrentThread();
+    PR_Lock(me->stateLock);
+    _PR_THREAD_BLOCK_INTERRUPT(me);
+    printf("Blocking interrupt for %lx, flags are %lx\n", me, me->flags);
+    PR_Unlock(me->stateLock);
+}  /* PR_BlockInterrupt */
+
+PR_IMPLEMENT(void) PR_UnblockInterrupt(void)
+{
+    PRThread *me = PR_CurrentThread();
+    PR_Lock(me->stateLock);
+    _PR_THREAD_UNBLOCK_INTERRUPT(me);
+    printf("Unblocking interrupt for %lx, flags are %lx\n", me, me->flags);
+    PR_Unlock(me->stateLock);
+}  /* PR_UnblockInterrupt */
+
+
+void _PR_InitThreads(PRThreadType type, PRThreadPriority priority, 
+    PRUintn maxPTDs)
 {
     /* Nothing to do */
 }
@@ -245,9 +355,9 @@ void _PR_InitStacks(void) {
 
 PR_IMPLEMENT(PRStatus) PR_Cleanup(void)
 {
+    PRThread *me = PR_CurrentThread();
+    PR_ASSERT(me == primordialThread);
     if (_pr_initialized) {
-        /* Clean up primorial thread */
-        procExit();
         _PR_CleanupSocket();
         _PR_CleanupMW();
         _PR_CleanupDtoa();
@@ -260,30 +370,48 @@ PR_IMPLEMENT(PRStatus) PR_Cleanup(void)
         _PR_CleanupIO();
         _PR_CleanupLayerCache();
         _PR_CleanupEnv();
-
+        /* Clean up primorial thread */
+        procExit();
+        _PR_Release_Memory();
         _pr_initialized = PR_FALSE;
     }
     return PR_SUCCESS;
 }  /* PR_Cleanup */
 
-void _PR_MD_Wait(PRThread *thread) {
-    ULONG mySig =  1 << thread->port->mp_SigBit;
-    Forbid();
-    if (SetSignal(0L, 0L) & mySig) {
-        /* Already got signal. I guess I was too slow to get to the wait */
-        thread->state = _PR_RUNNING;
-    } else {
-        if (thread->state == _PR_RUNNING) {
-            thread->state = _PR_SUSPENDED;
-        }
+void _PR_MD_Wait(PRThread *thread, PRBool interruptable) {
+    ULONG oldSigs;
+    ULONG mySig = 1 << thread->port->mp_SigBit;
+    PRBool blocked;
+
+    oldSigs = SetSignal(0L, 0L);
+#if 0
+    if (oldSigs & mySig) {
+        printf("HELP!!! %lx already got signal!!\n", thread);
+    }
+#endif
+    SetSignal(0, mySig);
+    if (interruptable == PR_TRUE) {
+        ULONG interruptSig =  1 << thread->interruptSignal;
+        ULONG sigs = mySig | interruptSig;
+
+        // Can't do this here: PR_Lock(thread->stateLock);
+        Forbid();
+        PR_ASSERT(thread->state != _PR_RUNNING);
+        blocked = (thread->flags & _PR_INTERRUPT_BLOCKED) ? PR_TRUE : PR_FALSE;
+        // Can't do this here: PR_Unlock(thread->stateLock);
+        Permit();        
+        Wait(sigs);
+    } else {        
         Wait(mySig);
     }
     SetSignal(0, mySig);
-    Permit();
 }
 
 void _PR_MD_Signal(PRThread *thread) {
+    PR_Lock(thread->stateLock);
+    PR_ASSERT(thread->state != _PR_RUNNING);
     thread->state = _PR_RUNNING;
+    PR_Unlock(thread->stateLock);
     Signal((struct Task *)thread->p, 1 << thread->port->mp_SigBit);
 }
 
